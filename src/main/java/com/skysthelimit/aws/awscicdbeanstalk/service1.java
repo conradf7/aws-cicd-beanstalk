0e3cd3843d952dd74cf1434371fd81c8f7093495
Conrad Fernandes <conradzen@gmail.com>
2019-04-29 15:17:25 -0400

    service1 route change for PRODUCTION

--- src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
+++ src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
@@ -19,7 +19,7 @@ public class service1 {
 
         String msg =         
             "<font size=\"7\">" +
-            "***** PROD ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk" + 
+            "***** PRODUCTION ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk" + 
             "</font>" +
             "<br>" +
             "<font size=\"6\">" +
