78cc041befd91ec12019e6868b41aba53d61be02
Conrad Fernandes <conradzen@gmail.com>
2019-04-30 00:49:09 -0400

    added integration feature-1

--- src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
+++ src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
@@ -17,7 +17,9 @@ public class service1 {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd  HH:mm:ss");
         String t  = dateFormat.format(new Date());
 
-        String msg =         
+        String msg =    
+            "<img src=\"https://www.cloudaxis.com/wp-content/uploads/2018/09/codebuild.png\" " +
+            "alt=\"AWS Codebuild\" width=\"120\" height=\"100\">" +       
             "<font size=\"7\">" +
             "***** INTEGRATION ... if approved -> to PROD ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk" + 
             "</font>" +
