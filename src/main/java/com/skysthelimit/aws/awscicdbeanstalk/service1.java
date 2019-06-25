b34cf74a14a890bca767b130fcea61a792249a78
Conrad Fernandes <conrad.fernandes@jhuapl.edu>
2019-06-25 11:18:20 -0400

    changed Tomcat webserver icon

--- src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
+++ src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
@@ -15,24 +15,18 @@ public class service1 {
     public String handle_service1() {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd  HH:mm:ss");
-        String t  = dateFormat.format(new Date());
+        String t = dateFormat.format(new Date());
 
-        String msg = 
-            "<body bgcolor=\"#E6E6FA\">" + 
-            "<img src=\"https://www.cloudaxis.com/wp-content/uploads/2018/09/codebuild.png\" " +
-            "alt=\"AWS Codebuild\" width=\"120\" height=\"100\">" +   
-            "<img src=\"https://cdn-images-1.medium.com/max/1600/1*6B-6_az43jpZIalwbsuoQw.png\" " +
-            "alt=\"Elastic Beanstalk\" width=\"110\" height=\"100\">" +   
-            "<img src=\"https://www.lucidtechsystems.com/wp-content/uploads/2017/01/apache-tomcate-web-server-online-training.jpg\" " +
-            "alt=\"Apache Tomcat\" width=\"100\" height=\"100\">" +     
-            "<br>" +              
-            "<font size=\"7\">" +
-            "***** PRODUCTION -- added feature 3 ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk" + 
-            "</font>" +
-            "<br>" +
-            "<font size=\"6\">" +
-            "The current UTC time is:  " + "<b>" + t + "</b>" + 
-            "</font>" ;
+        String msg = "<body bgcolor=\"#E6E6FA\">"
+                + "<img src=\"https://www.cloudaxis.com/wp-content/uploads/2018/09/codebuild.png\" "
+                + "alt=\"AWS Codebuild\" width=\"120\" height=\"100\">"
+                + "<img src=\"https://cdn-images-1.medium.com/max/1600/1*6B-6_az43jpZIalwbsuoQw.png\" "
+                + "alt=\"Elastic Beanstalk\" width=\"110\" height=\"100\">"
+                + "<img src=\"https://www.globalonlinetrainings.com/wp-content/uploads/2015/10/apache-tomcate-web-server-online-training.jpg\" "
+                + "alt=\"Apache Tomcat\" width=\"100\" height=\"100\">" + "<br>" + "<font size=\"7\">"
+                + "***** PRODUCTION -- added feature 3 ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk"
+                + "</font>" + "<br>" + "<font size=\"6\">" + "The current UTC time is:  " + "<b>" + t + "</b>"
+                + "</font>";
 
         return msg;
     }
