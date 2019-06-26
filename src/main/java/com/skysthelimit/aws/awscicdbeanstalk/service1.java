b8fa432d139954d9a0b0d76dd62364ee405f4244
Conrad Fernandes <conrad.fernandes@jhuapl.edu>
2019-06-25 23:37:22 -0400

    changed tomcat icon

--- src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
+++ src/main/java/com/skysthelimit/aws/awscicdbeanstalk/service1.java
@@ -15,26 +15,20 @@ public class service1 {
     public String handle_service1() {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd  HH:mm:ss");
-        String t  = dateFormat.format(new Date());
+        String t = dateFormat.format(new Date());
 
-        String msg =    
-            "<body bgcolor=\"#FFA07A\">" + 
-            "<img src=\"https://cdn-images-1.medium.com/max/1200/1*jMQ9lkY5SBnbcOlJB4aizg.png\" " +
-            "alt=\"AWS Codebuild\" width=\"240\" height=\"100\">" +   
-            "<img src=\"https://www.cloudaxis.com/wp-content/uploads/2018/09/codebuild.png\" " +
-            "alt=\"AWS Codebuild\" width=\"120\" height=\"100\">" +               
-            "<img src=\"https://cdn-images-1.medium.com/max/1600/1*6B-6_az43jpZIalwbsuoQw.png\" " +
-            "alt=\"Elastic Beanstalk\" width=\"110\" height=\"100\">" +   
-            "<img src=\"https://www.lucidtechsystems.com/wp-content/uploads/2017/01/apache-tomcate-web-server-online-training.jpg\" " +
-            "alt=\"Apache Tomcat\" width=\"100\" height=\"100\">" +     
-            "<br>" +            
-            "<font size=\"7\">" +
-            "***** INTEGRATION ... Added feature 6.  If approved, promote -> to PROD ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk" + 
-            "</font>" +
-            "<br>" +
-            "<font size=\"6\">" +
-            "The current UTC time is: " + "<b>" + t + "</b>" + 
-            "</font>" ;
+        String msg = "<body bgcolor=\"#FFA07A\">"
+                + "<img src=\"https://cdn-images-1.medium.com/max/1200/1*jMQ9lkY5SBnbcOlJB4aizg.png\" "
+                + "alt=\"AWS Codebuild\" width=\"240\" height=\"100\">"
+                + "<img src=\"https://www.cloudaxis.com/wp-content/uploads/2018/09/codebuild.png\" "
+                + "alt=\"AWS Codebuild\" width=\"120\" height=\"100\">"
+                + "<img src=\"https://cdn-images-1.medium.com/max/1600/1*6B-6_az43jpZIalwbsuoQw.png\" "
+                + "alt=\"Elastic Beanstalk\" width=\"110\" height=\"100\">"
+                + "<img src=\"https://noticiasseguridad.com/nsnews_u/2019/04/tomcat.png\" "
+                + "alt=\"Apache Tomcat\" width=\"100\" height=\"100\">" + "<br>" + "<font size=\"7\">"
+                + "***** INTEGRATION ... Added feature 6.  If approved, promote -> to PROD ****  CodePipeline with CI/CD for Tomcat Java App deployed via Elastic Beanstalk"
+                + "</font>" + "<br>" + "<font size=\"6\">" + "The current UTC time is: " + "<b>" + t + "</b>"
+                + "</font>";
 
         return msg;
     }
